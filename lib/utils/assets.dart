fb654e52b1e533031046bdac2bff3c168a68b06e
NoorMustafa4556 <noormustafa4556@gmail.com>
2025-10-20 18:17:39 +0500

    first commit

---
+++ lib/utils/assets.dart
@@ -0,0 +1,13 @@
+class StaticAssets {
+  static const String logo = 'assets/images/logo.png'; // assets/ prefix add kiya
+  static const String gradLogo = 'assets/images/grad_logo.png'; // assets/ prefix add kiya
+
+  static const String kaba = 'assets/images/kaba.png';
+  static const String roza = 'assets/images/roza.png';
+  static const String sajda = 'assets/images/sajda.jpg';
+  static const String quranRail = 'assets/images/quranRail.png';
+
+  static const String ui = 'assets/images/ui.png';
+  static const String easyNav = 'assets/images/easyNav.png';
+  static const String drawer3d = 'assets/images/drawer3d.gif';
+}
---
+++ lib/widgets/calligraphy.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+import '../configs/app_dimensions.dart';
+import '../utils/assets.dart';
+
+class Calligraphy extends StatelessWidget {
+  const Calligraphy({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Positioned(
+      right: width * 0.01,
+      top: height * 0.045,
+      child: Image.asset(
+        StaticAssets.gradLogo,
+        height: AppDimensions.normalize(75),
+      ),
+    );
+  }
+}
